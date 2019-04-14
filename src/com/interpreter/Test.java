85efbd7b5edd529a1d40caeffd7da36cb1866f3f
notn0name <48620313+notn0name@users.noreply.github.com>
2019-04-14 23:46:35 +0300

    Update Test.java

--- src/com/interpreter/Test.java
+++ src/com/interpreter/Test.java
@@ -3,6 +3,6 @@ package com.interpreter;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Фёдор - петух");
+        System.out.println("Lbvf - петух");
     }
 }
