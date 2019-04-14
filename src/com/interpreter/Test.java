7aff99832bdfc923c7b41aa02c850b4aa9c5cec6
Dmitriy Titov <titov.dmitriy2000@yandex.ru>
2019-04-14 23:49:39 +0300

    commit to fedor2

--- src/com/interpreter/Test.java
+++ src/com/interpreter/Test.java
@@ -3,6 +3,6 @@ package com.interpreter;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Lbvf - петух");
+        System.out.println("Фёдор - петух");
     }
 }
